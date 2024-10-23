fb10d3684c49cbd528606167a23655d5384b68ed
Guzel <grkamalova@edu.hse.ru>
2024-10-23 20:41:10 +0300

    string processing

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -10,29 +10,10 @@
 #include <utility>
 #include <vector>
 #include "read_input_functions.h"
+#include "string_processing.h"
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
-
-vector<string> SplitIntoWords(const string& text) {
-    vector<string> words;
-    string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        } else {
-            word += c;
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
-
-    return words;
-}
 
 struct Document {
     Document() = default;
